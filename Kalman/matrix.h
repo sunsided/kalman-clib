ae56553a7401c9efb8eb13d2d6ad5f5646f9af1f
Markus Mayer <development@widemeadows.de>
2014-01-10 17:18:21 +0100

    added comment for the matrix struct

--- Kalman/matrix.h
+++ Kalman/matrix.h
@@ -3,11 +3,28 @@
 
 #include <stdint.h>
 
+/**
+* Matrix data type definition.
+*/
 typedef float matrix_data_t;
 
+/**
+* \brief Matrix definition
+*/
 typedef struct {
+    /**
+    * \brief Number of rows
+    */
     uint_fast8_t rows;
+
+    /**
+    * \brief Number of columns
+    */
     uint_fast8_t cols;
+
+    /**
+    * \brief Pointer to the data array of size {\see rows} x {\see cols}.
+    */
     matrix_data_t *data;
 } matrix_t;
 
